5daaf34a48ab65972e9fe857d0629cfc3deed4db
Fiuse <Fiw0202@hotmail.com>
2024-06-06 22:46:09 +0700

    ใช้งาน Service authMe ที่ layout
    - ใช้งาน Service authMe ที่ layout

--- src/app/(Home)/layout.tsx
+++ src/app/(Home)/layout.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { styled, Container, Box } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "@/app/(Home)/layout/header/Header";
 import Sidebar from "@/app/(Home)/layout/sidebar/Sidebar";
 import theme from "@/utils/theme";
+import { userContext } from "@/utils/context/usercontext";
+import { authMe } from "@/utils/service/login";
+import { IRespMeData } from "@/utils/service/login/interface";
 
 const PageWrapper = styled("div")(() => ({
   display: "flex",
@@ -22,29 +25,47 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
   const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+  const [userData, setUserData] = useState<IRespMeData>();
+
+  const checkAuth = async () => {
+    try {
+      const data = await authMe();
+      setUserData(data.result);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <>
-      {/* Header */}
-      <Header toggleMobileSidebar={() => setMobileSidebarOpen(true)} />
+      <userContext.Provider value={userData}>
+        {/* Header */}
+        <Header toggleMobileSidebar={() => setMobileSidebarOpen(true)} />
 
-      {/* Main Wrapper */}
-      <PageWrapper className="page-wrapper">
-        {/* Sidebar */}
-        <Sidebar
-          isSidebarOpen={isSidebarOpen}
-          isMobileSidebarOpen={isMobileSidebarOpen}
-          onSidebarClose={() => setMobileSidebarOpen(false)}
-        />
-        {/* PageContent */}
-        <Container
-          sx={{
-            paddingTop: "20px",
-            maxWidth: "1200px",
-          }}
-        >
-          <Box sx={{ minHeight: "calc(100vh - 170px)" }}>{children}</Box>
-        </Container>
-      </PageWrapper>
+        {/* Main Wrapper */}
+        <PageWrapper className="page-wrapper">
+          {/* Sidebar */}
+          <Sidebar
+            isSidebarOpen={isSidebarOpen}
+            isMobileSidebarOpen={isMobileSidebarOpen}
+            onSidebarClose={() => setMobileSidebarOpen(false)}
+          />
+          {/* PageContent */}
+          <Container
+            sx={{
+              paddingTop: "20px",
+              maxWidth: "1200px",
+            }}
+          >
+            <Box sx={{ minHeight: "calc(100vh - 170px)" }}>{children}</Box>
+          </Container>
+        </PageWrapper>
+      </userContext.Provider>
     </>
   );
 }
