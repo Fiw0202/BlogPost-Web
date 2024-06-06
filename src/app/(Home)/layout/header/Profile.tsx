28b3e001e1c5dd0f4589f827f94cf93e1f9e0d92
Fiuse <Fiw0202@hotmail.com>
2024-06-06 22:48:05 +0700

    นำ context user ไปใช้
    - นำ context user ไปใช้กับ profile
    - ปรับ Avatar ให้แสดงด้วยตัวอักษรแรกของชื่อ

--- src/app/(Home)/layout/header/Profile.tsx
+++ src/app/(Home)/layout/header/Profile.tsx
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useTheme } from "@mui/material/styles";
 import { Box, Avatar, Typography, Grid } from "@mui/material";
+import { userContext } from "@/utils/context/usercontext";
 
 const Profile = () => {
   const theme = useTheme();
+  const user = useContext(userContext);
+
+  const getAvatarText = (name: string | undefined) => {
+    return name ? name.charAt(0).toUpperCase() : "";
+  };
 
   return (
     <Grid container>
-      <Grid item mr={2}>
-        <Box
-          sx={{
-            display: {
-              xs: "none",
-              sm: "flex",
-            },
-            alignItems: "center",
-          }}
+      <Grid
+        item
+        mr={2}
+        sx={{ display: "flex", alignItems: "center", alignContent: "center" }}
+      >
+        <Typography
+          variant="h5"
+          fontWeight="700"
+          color={theme.palette.primary.contrastText}
         >
-          <Typography
-            variant="h5"
-            fontWeight="700"
-            color={theme.palette.primary.contrastText}
-          >
-            Julia
-          </Typography>
-        </Box>
+          {user?.displayName}
+        </Typography>
       </Grid>
       <Grid item>
         <Avatar
-          src={"/images/users/user2.jpg"}
-          alt={"ProfileImg"}
+          alt="ProfileImg"
           sx={{
             width: 30,
             height: 30,
+            backgroundColor: theme.palette.primary.light,
+            color: theme.palette.text.primary,
           }}
-        />
+        >
+          {getAvatarText(user?.displayName)}
+        </Avatar>
       </Grid>
     </Grid>
   );
