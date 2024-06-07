a02004d19ea22a163bd233acb09a08993add82ff
Sadayu <sadayu@iconframework.com>
2024-06-07 17:08:15 +0700

    สร้าง Service Post จาก API
    - getAllPost
    - getPostById
    - getPostByUserId
    - createPost
    - updatePost

---
+++ src/utils/service/post/index.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+import {
+  IReqCreatePost,
+  IReqUpdatePost,
+  IRespAllPost,
+  IRespPost,
+} from "./interface";
+
+export const getAllPost = async () => {
+  try {
+    const req = await axios.get<unknown, IRespAllPost>(
+      `http://localhost:4000/master-post`
+    );
+    return req.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getPostById = async (id: string) => {
+  try {
+    const req = await axios.get<unknown, IRespPost>(
+      `http://localhost:4000/master-post/${id}`
+    );
+    return req.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getPostByUserId = async (id: string) => {
+  try {
+    const req = await axios.get<unknown, IRespPost>(
+      `http://localhost:4000/master-post/user/${id}`
+    );
+    return req.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createPost = async (req: IReqCreatePost) => {
+  try {
+    const resp = await axios.post<IReqCreatePost, IRespPost>(
+      `http://localhost:4000/master-post/`,
+      req
+    );
+    return resp.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updatePost = async (req: IReqUpdatePost) => {
+  try {
+    const resp = await axios.patch<IReqCreatePost, IRespPost>(
+      `http://localhost:4000/master-post/`,
+      req
+    );
+    return resp.data;
+  } catch (error) {
+    throw error;
+  }
+};
---
+++ src/utils/service/post/interface/index.ts
@@ -0,0 +1,29 @@
+import { IRespData } from "../../interface";
+
+export interface IRespPostData {
+  _id: string;
+  userId: string;
+  subject: string;
+  content: string;
+  groupPost: string;
+  createName: string;
+  createDate: string;
+}
+
+export interface IReqCreatePost {
+  userId: string;
+  subject: string;
+  content: string;
+  groupPost: string;
+}
+
+export interface IReqUpdatePost {
+  _id: string;
+  userId: string;
+  subject: string;
+  content: string;
+  groupPost: string;
+}
+
+export type IRespAllPost = IRespData<IRespPostData[]>;
+export type IRespPost = IRespData<IRespPostData>;
