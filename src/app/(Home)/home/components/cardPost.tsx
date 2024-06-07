4e9823926f3fcd2c4537e20797907b46f1d20485
Fiuse <Fiw0202@hotmail.com>
2024-06-07 21:27:22 +0700

    Update cardPost.tsx
    แสดงข้อมูลจาก API ใส่ component cardPost

--- src/app/(Home)/home/components/cardPost.tsx
+++ src/app/(Home)/home/components/cardPost.tsx
@@ -1,44 +1,62 @@
+"use client";
+
+import { GetAvatarText } from "@/utils/function/avatarText";
+import { IRespPostData } from "@/utils/service/post/interface";
 import {
   Avatar,
+  Box,
   Card,
   CardActions,
   CardContent,
   CardHeader,
+  Chip,
   Grid,
   IconButton,
   Typography,
 } from "@mui/material";
 import { IconMessage } from "@tabler/icons-react";
 
-const CardPost = () => {
+type TCardProps = {
+  data: IRespPostData[] | undefined;
+};
+
+const CardPost = ({ data }: TCardProps) => {
   return (
     <>
-      <Card sx={{ width: "100%" }}>
-        <CardHeader
-          avatar={<Avatar aria-label="recipe">S</Avatar>}
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <Grid container spacing={1}>
-            <Grid item>
-              <IconButton aria-label="comments">
-                <IconMessage />
-              </IconButton>
+      {data?.map((m, index) => (
+        <Card key={index} sx={{ width: "100%" }}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="recipe">
+                {GetAvatarText(m?.createName)}
+              </Avatar>
+            }
+            title={m?.subject}
+            subheader="September 14, 2016"
+          />
+          <Box paddingLeft="30px">
+            <Chip label={m?.groupPost} />
+          </Box>
+
+          <CardContent sx={{ paddingTop: 2 }}>
+            <Typography variant="body2" color="text.secondary">
+              {m?.content}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <Grid container spacing={1}>
+              <Grid item>
+                <IconButton aria-label="comments">
+                  <IconMessage />
+                </IconButton>
+              </Grid>
+              <Grid item alignContent={"center"}>
+                <Typography variant="subtitle2"> 32 comments</Typography>
+              </Grid>
             </Grid>
-            <Grid item alignContent={"center"}>
-              <Typography variant="subtitle2"> 32 comments</Typography>
-            </Grid>
-          </Grid>
-        </CardActions>
-      </Card>
+          </CardActions>
+        </Card>
+      ))}
     </>
   );
 };
