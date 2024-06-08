7f4c4c305e25007078fb1e9cd3d552c7cd8e25e2
Fiuse <Fiw0202@hotmail.com>
2024-06-08 09:27:01 +0700

    Create group.ts
    สร้าง Group เป็นไฟล์เเยก

---
+++ src/app/(Home)/Mockup/group.ts
@@ -0,0 +1,10 @@
+export const Group = [
+    { value: "", label: "None" },
+    { value: "History", label: "History" },
+    { value: "Food", label: "Food" },
+    { value: "Pets", label: "Pets" },
+    { value: "Health", label: "Health" },
+    { value: "Fashion", label: "Fashion" },
+    { value: "Exercise", label: "Exercise" },
+    { value: "Others", label: "Others" },
+  ];
