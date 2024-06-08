7583cf4ee2c8a12a91a2d9ba75405fdc1df587d5
Fiuse <Fiw0202@hotmail.com>
2024-06-08 09:30:47 +0700

    Create cardPost.tsx
    - เพิ่ม component cardPost สำหรับหน้า our-page

---
+++ src/app/(Home)/our-blog/components/cardPost.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { GetAvatarText } from "@/utils/function/avatarText";
+import { IRespPostData } from "@/utils/service/post/interface";
+import theme from "@/utils/theme";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Chip,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { IconMessage } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+type TCardProps = {
+  data: IRespPostData[] | undefined;
+};
+
+const CardPost = ({ data }: TCardProps) => {
+  dayjs.extend(relativeTime);
+  const router = useRouter();
+
+  return (
+    <>
+      {data?.map((m, index) => (
+        <Card
+          key={index}
+          sx={{
+            width: "100%",
+            "&:hover": {
+              cursor: "pointer",
+            },
+          }}
+          onClick={() => router.push(`home/${m._id}`)}
+        >
+          <CardHeader
+            avatar={
+              <Avatar aria-label="recipe">
+                {GetAvatarText(m?.createName)}
+              </Avatar>
+            }
+            title={m?.createName}
+            subheader={dayjs(new Date()).to(m?.createDate, true)}
+          />
+          <Box paddingLeft="30px">
+            <Chip label={m?.groupPost} />
+          </Box>
+
+          <CardContent sx={{ paddingTop: 2 }}>
+            <Typography
+              variant="h4"
+              fontWeight={700}
+              color={theme.palette.text.primary}
+              pb={1}
+            >
+              {m?.subject}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {m?.content}
+            </Typography>
+          </CardContent>
+          {/* <CardActions disableSpacing>
+            <Grid container spacing={1}>
+              <Grid item>
+                <IconButton aria-label="comments">
+                  <IconMessage />
+                </IconButton>
+              </Grid>
+              <Grid item alignContent={"center"}>
+                <Typography variant="subtitle2"> 32 comments</Typography>
+              </Grid>
+            </Grid>
+          </CardActions> */}
+        </Card>
+      ))}
+    </>
+  );
+};
+
+export default CardPost;
